ae6f251d6574241c94ef0fbcc733959b580cdd7d
v1n1c1u <vinicius.abner1039@gmail.com>
2025-03-24 21:05:22 -0700

    chore: Removes unused function from SignUp.jsx

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -30,7 +30,6 @@ export const SignUp = (props) => {
 
   const [showPassword, setShowPassword] = useState(false);
   const handleClickShowPassword = () => setShowPassword(!showPassword);
-  const handleMouseDownPassword = () => setShowPassword(!showPassword);
 
   // Initialization
   useEffect(() => {
